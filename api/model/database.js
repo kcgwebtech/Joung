e97e8698e4cb78d22f7d642469fc13f7a012712a
Gimyoo <5012s@kookmin.ac.kr>
2018-10-28 16:31:30 +0900

    api

--- api/model/database.js
+++ api/model/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const database = new Sequelize(
     'aacount',
     'root',
-    '',
+    '5012s',
     {
         host:'localhost',
         dialect:'mysql'
