3e49f62143eaeb2650057761898d12c4bb4d8b52
Gimyoo <5012s@kookmin.ac.kr>
2018-11-25 14:17:14 +0900

    api 배포

--- api/auth/index.js
+++ api/auth/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.use('/register', require('./register'));
 router.use('/login', require('./login'));
+router.use('/logout', require('./logout'));
 
 module.exports = router;
 
